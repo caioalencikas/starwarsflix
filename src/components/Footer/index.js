1c23d5ee4de762839bb4fe951dc44cb220ac9050
Caio Alencikas <caio.alencikas@gmail.com>
2020-08-14 20:37:41 -0300

    Footer Star Wars and colors

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/logo-footer.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/caioalencikas">
+        <img src={Logo} alt="Logo Star Wars" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        May the force be with you. By
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        <a href="https://www.linkedin.com/in/caioalencikas/">
+          Caio Alencikas
         </a>
       </p>
     </FooterBase>
